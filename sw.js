3a6af408d5ce28a09b18b90d020300bf985d0ec4
thormann <beratung@tomhormann.de>
2026-07-06 20:21:06 +0200

    Event-Nachfrage-Button + offene Nachfragen (v2.33)

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Firebase-Live-Traffic (Sync/Chat): NIE abgefangen -> braucht echtes Netz.
    - Karten-Kacheln: network, offline Rückfall auf bereits Gesehenes.
 */
-const VERSION = '2.32';
+const VERSION = '2.33';
 const CACHE = 'agraffen-' + VERSION;
 const CDN = ['unpkg.com', 'www.gstatic.com'];
 
