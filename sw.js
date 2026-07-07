75e56c63f935c56018a58e52aabfe7abe18d5af5
thormann <beratung@tomhormann.de>
2026-07-07 15:40:02 +0200

    v2.40: Medien direkt (unverschluesselt, geheimer Pfad)

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Firebase-Live-Traffic (Sync/Chat): NIE abgefangen -> braucht echtes Netz.
    - Karten-Kacheln: network, offline Rückfall auf bereits Gesehenes.
 */
-const VERSION = '2.39';
+const VERSION = '2.40';
 const CACHE = 'agraffen-' + VERSION;
 const CDN = ['unpkg.com', 'www.gstatic.com'];
 
