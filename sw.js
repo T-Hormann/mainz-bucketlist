4228649cd3665da88481f856b9f2d3b9b54b6f6c
thormann <beratung@tomhormann.de>
2026-07-09 10:32:07 +0200

    v2.41 Etappe 1: neue Palette + Agraffe-Logo

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
    - Firebase-Live-Traffic (Sync/Chat): NIE abgefangen -> braucht echtes Netz.
    - Karten-Kacheln: network, offline Rückfall auf bereits Gesehenes.
 */
-const VERSION = '2.40';
+const VERSION = '2.41';
 const CACHE = 'agraffen-' + VERSION;
 const CDN = ['unpkg.com', 'www.gstatic.com'];
 
